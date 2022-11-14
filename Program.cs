e41defeae14be620701a1eefce3d368f063adfa5
Azizbek Olimov <aziz.olim3008@gmail.com>
2022-11-14 14:31:44 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace UserInfo 
+{
+    struct UserDetails
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name, please: ");
+            string userName = Console.ReadLine();
+
+            Console.Write("please, input your surname: ");
+            string userSurname = Console.ReadLine();
+
+            Console.Write("Please, enter your city of birth: ");
+            string city = Console.ReadLine();
+
+            Console.Write("please, enter your Age: ");
+            int userAge = Convert.ToInt32(Console.ReadLine());
+
+
+
+            Console.WriteLine($"Details of the User are name: {userName} surname:{userSurname} city:{city} age:{userAge} ");
+
+        }
+    }
+}
